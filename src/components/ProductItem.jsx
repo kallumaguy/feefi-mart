aee4602e62848fa8d248c5a1a5cc0dade9630f78
kallumaguy <adilahmed.aa786@gmail.com>
2025-05-05 16:25:41 +0530

    day 3 commit

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -9,7 +9,7 @@ const ProductItem = ({id, image, name, price}) => {
   return (
     <Link className='text-gray-700 curser-pointer' to={`/product/${id}`}>
         <div className='overflow-hidden'>
-            <img src={image[0]} alt={name} className='hover:scale-100 transition ease-in-out' />
+            <img src={image[0]} alt={name} className='hover:scale-105 transition ease-in-out' />
         </div>
         <p className='pt-3 pb-1 text-sm'>{name}</p>
         <p className='text-sm font-medium'>{currency}{price}</p>
