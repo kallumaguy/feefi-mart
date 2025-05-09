36338a5655cf2e3723f921fa479f42826fa681ca
kallumaguy <adilahmed.aa786@gmail.com>
2025-05-09 18:33:25 +0530

    day 6,7 commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { ShopContext } from '../context/ShopContext';
 const Navbar = () => {
 
   const [visible, setVisible] = useState(false);
-  const {setShowSearch, getCartCount} = useContext(ShopContext);
+  const { setShowSearch, getCartCount } = useContext(ShopContext);
 
 
   return (
@@ -36,9 +36,11 @@ const Navbar = () => {
       </ul>
 
       <div className='flex itrems-center gap-6'>
-        <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 h-5 cursor-pointer' alt="" />
+        <img onClick={() => setShowSearch(true)} src={assets.search_icon} className='w-5 h-5 cursor-pointer' alt="" />
         <div className='group relative'>
-          <img className='w-5 h-5 cursor-pointer' src={assets.profile_icon} alt="" />
+          <Link to={'/login'}>
+            <img className='w-5 h-5 cursor-pointer' src={assets.profile_icon} alt="" />
+          </Link>
           <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
             <div className='flex flex-col gap-2 w-36 py-5 px-5 bg-slate-100 text-gray-500 rounded'>
               <p className='cursor-pointer hover:text-black'>My Profile</p>
